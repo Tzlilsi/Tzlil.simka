587a085060c071802273c7a82af7b0d8082dc562
Tzlilsi <tzlilsi@post.bgu.ac.il>
2021-12-01 17:30:59 +0200

    class6

---
+++ CV.js
@@ -0,0 +1,10 @@
+const d = new Date();
+    console.log(d);
+    var h = d.getHours();
+    
+function validEmail(EmailC, note1){
+        const inpObj= document.getElementById("EmailC");
+        if(!inpObg.checkValidity()){
+            document.getElementById(note1).innerHTML= inpObj.validationMwssage;
+        }
+    }
